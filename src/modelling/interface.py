8061fd6295e3dfc0617c4c8f00127fe09787cb5e
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-07-12 22:31:33 +0100

    Update src/modelling/interface.py

--- src/modelling/interface.py
+++ src/modelling/interface.py
@@ -1,24 +1,24 @@
+"""Module interface.py"""
 import logging
+
 import datasets
 
-import config
-import src.modelling.t5.preprocessing
+import src.modelling.t5.splittings
+
 
 class Interface:
+    """
+    An interface to one or more model development packages
+    """
 
     def __init__(self, source: datasets.Dataset):
         """
 
-        :param source:
+        :param source: A datasets.Dataset data piece
         """
 
         self.__source = source
-
-        # Configurations
-        self.__configurations = config.Config()
-
-        # Instances
-        self.__preprocessing = src.modelling.t5.preprocessing.Preprocessing()
+        self.__splits: datasets.DatasetDict = src.modelling.t5.splittings.Splittings(source=source).__call__()
 
         # Logging
         logging.basicConfig(level=logging.INFO,
@@ -26,29 +26,10 @@ class Interface:
                             datefmt='%Y-%m-%d %H:%M:%S')
         self.__logger = logging.getLogger(__name__)
 
-    def __split(self) -> datasets.DatasetDict:
-        """
-
-        :return:
-        """
-
-        splits: datasets.DatasetDict = self.__source.train_test_split(
-            test_size=self.__configurations.test_fraction)
-
-        return splits
-
     def exc(self):
         """
 
         :return:
         """
 
-        # Splitting the data into training & testing sets; each split is a datasets.arrow_dataset.Dataset, and
-        # each instance of a split has keys -> 'text', 'summary', 'title'
-        splits: datasets.DatasetDict = self.__split()
-        self.__logger.info(splits.keys())
-        self.__logger.info(splits['train'][0].keys())
-
-        # Converting each split to a tokenized split
-        cuts: datasets.DatasetDict = splits.map(self.__preprocessing.exc, batched=True)
-        self.__logger.info(cuts.keys())
+        self.__logger.info(self.__splits.keys())
