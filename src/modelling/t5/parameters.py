98aa7240aba5ae4e6ed35d31d6b959fb817da431
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-07-12 20:15:54 +0100

    Add src/modelling/t5/parameters.py

---
+++ src/modelling/t5/parameters.py
@@ -0,0 +1,12 @@
+import transformers
+
+class Parameters:
+
+    def __init__(self):
+
+        self.max_length_input = 1024
+        self.max_length_target = 128
+
+        checkpoint = 'google-t5/t5-small'
+        self.tokenizer: transformers.PreTrainedTokenizerFast = (
+            transformers.AutoTokenizer.from_pretrained(pretrained_model_name_or_path=checkpoint))
