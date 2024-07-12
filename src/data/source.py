ddeb939ce2e51b0645d35d4d26ec191acf3132f9
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-07-12 18:19:00 +0100

    Update src/data/source.py

--- src/data/source.py
+++ src/data/source.py
@@ -5,6 +5,12 @@ import datasets
 class Source:
 
     def __init__(self):
+        """
+        Constructor
+        """
+
+        # The Data
+        self.__dataset: datasets.DatasetDict = datasets.load_dataset('billsum')
 
         # Logging
         logging.basicConfig(level=logging.INFO,
@@ -12,9 +18,18 @@ class Source:
                             datefmt='%Y-%m-%d %H:%M:%S')
         self.__logger = logging.getLogger(__name__)
 
+    def __temporary(self):
+
+        return self.__dataset['ca_test']
+
     def exc(self):
 
-        dataset: datasets.DatasetDict = datasets.load_dataset('billsum')
-        self.__logger.info(type(dataset))
-        self.__logger.info(dataset.keys())
+        # The data segments
+        self.__logger.info('The data segments:\n%s', self.__dataset.keys())
 
+        # The initial focus
+        self.__logger.info('Focusing on data segment <ca_test>\n')
+        temporary: datasets.Dataset = self.__temporary()
+        self.__logger.info(temporary.__dir__())
+
+        return temporary
