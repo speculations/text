1b4d08a99838d0494947f3088fd06f0dfecbcb84
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-07-12 20:39:29 +0100

    Update src/modelling/t5/preprocessing.py

--- src/modelling/t5/preprocessing.py
+++ src/modelling/t5/preprocessing.py
@@ -1,4 +1,6 @@
 import logging
+
+import datasets.formatting.formatting
 import transformers
 
 import src.modelling.t5.parameters
@@ -20,17 +22,18 @@ class Preprocessing:
                             datefmt='%Y-%m-%d %H:%M:%S')
         self.__logger = logging.getLogger(__name__)
 
-    def exc(self, blob):
+    def exc(self, blob: datasets.formatting.formatting.LazyBatch) -> transformers.BatchEncoding:
 
         self.__logger.info(type(blob))
 
         # Independent Variable
         inputs = [self.__prefix + segment for segment in blob['text']]
-        structure = self.__tokenizer(text=inputs, max_length=self.__max_length_input, truncation=True)
+        structure: transformers.BatchEncoding = self.__tokenizer(text=inputs, max_length=self.__max_length_input, truncation=True)
         self.__logger.info(type(structure))
 
         # Dependent Variable
         targets = self.__tokenizer(text_target=blob['summary'], max_length=self.__max_length_target, truncation=True)
         structure['labels']  = targets['input_ids']
+        self.__logger.info(type(structure))
 
         return structure
