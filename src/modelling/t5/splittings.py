54c265981b9f6ecd1c686c47ef2fd3fc98b4499a
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-07-12 23:37:15 +0100

    Update src/modelling/t5/splittings.py

--- src/modelling/t5/splittings.py
+++ src/modelling/t5/splittings.py
@@ -10,32 +10,16 @@ class Splittings:
     This class splits a datasets.Dataset into tokenized training & testing sets
     """
 
-    def __init__(self, source: datasets.Dataset):
+    def __init__(self, splits: datasets.DatasetDict):
         """
 
-        :param source: A datasets.Dataset data piece
+        :param splits: Training, ...
         """
 
-        self.__source = source
-
-        # Configurations
-        self.__configurations = config.Config()
+        self.__splits = splits
 
         # Instances
         self.__preprocessing = src.modelling.t5.preprocessing.Preprocessing()
-
-    def __preliminary(self) -> datasets.DatasetDict:
-        """
-        Splitting the data into training & testing sets; each split is a datasets.arrow_dataset.Dataset, and
-        each instance of a split has keys -> 'text', 'summary', 'title'
-
-        :return:
-        """
-
-        splits: datasets.DatasetDict = self.__source.train_test_split(
-            test_size=self.__configurations.test_fraction)
-
-        return splits
 
     def __call__(self) -> datasets.DatasetDict:
         """
@@ -43,8 +27,5 @@ class Splittings:
         :return:
         """
 
-        # Data splitting
-        splits: datasets.DatasetDict = self.__preliminary()
-
-        # Converting each split into a tokenized split
-        return splits.map(self.__preprocessing.exc, batched=True)
+        # Converting each split into a T5 tokenized split
+        return self.__splits.map(self.__preprocessing.exc, batched=True)
