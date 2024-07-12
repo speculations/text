47eb74acddf73cb9f1b0ee4dab790cc1d390aaed
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-07-12 18:44:48 +0100

    Update src/modelling/interface.py

--- src/modelling/interface.py
+++ src/modelling/interface.py
@@ -5,8 +5,15 @@ import config
 
 class Interface:
 
-    def __init__(self):
+    def __init__(self, source: datasets.Dataset):
+        """
 
+        :param source:
+        """
+
+        self.__source = source
+
+        # Configurations
         self.__configurations = config.Config()
 
         # Logging
@@ -15,7 +22,24 @@ class Interface:
                             datefmt='%Y-%m-%d %H:%M:%S')
         self.__logger = logging.getLogger(__name__)
 
-    def exc(self, source: datasets.Dataset):
+    def __split(self):
+        """
 
-        splits: datasets.DatasetDict = source.train_test_split(test_size=self.__configurations.test_fraction)
+        :param splits:
+        :return:
+        """
+
+        splits: datasets.DatasetDict = self.__source.train_test_split(
+            test_size=self.__configurations.test_fraction)
+
         self.__logger.info(splits.keys())
+        self.__logger.info(splits['train'].shape)
+        self.__logger.info(splits['train'][0].keys())
+
+        return splits
+
+    def exc(self):
+
+        splits = self.__split()
+        self.__logger.info(splits['train'][0])
+
