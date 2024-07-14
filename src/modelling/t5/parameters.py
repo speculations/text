b44aa6dc9125fd74a6ce0142d8e206c3dca22690
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-07-14 18:21:53 +0100

    Update src/modelling/t5/parameters.py

--- src/modelling/t5/parameters.py
+++ src/modelling/t5/parameters.py
@@ -1,3 +1,4 @@
+import torch
 import transformers
 
 class Parameters:
@@ -6,6 +7,8 @@ class Parameters:
 
         self.max_length_input = 1024
         self.max_length_target = 128
+
+        self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
         self.checkpoint = 'google-t5/t5-small'
         self.tokenizer: transformers.PreTrainedTokenizerFast = (
