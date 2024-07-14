ac52b1cce43201b91405d4c41c85a27b80ce2631
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-07-14 16:50:22 +0100

    Update src/modelling/t5/steps.py

--- src/modelling/t5/steps.py
+++ src/modelling/t5/steps.py
@@ -1,8 +1,12 @@
 import logging
 
 import datasets
+import transformers
+
+import src.elements.variable as vr
 
 import src.modelling.t5.splittings
+import src.modelling.t5.intelligence
 
 
 class Steps:
@@ -14,6 +18,10 @@ class Steps:
         """
 
         self.__splits = splits
+
+        # A set of values for machine learning model development
+        self.__variable = vr.Variable()
+        self.__variable._replace(EPOCHS=5)
 
         # Logging
         logging.basicConfig(level=logging.INFO,
@@ -30,3 +38,8 @@ class Steps:
         # T5 tokenized training & testing splits
         data: datasets.DatasetDict = src.modelling.t5.splittings.Splittings(splits=self.__splits).__call__()
         self.__logger.info(data.keys())
+
+        intelligence = src.modelling.t5.intelligence.Intelligence(variable=self.__variable)
+        model: transformers.Seq2SeqTrainer = intelligence(data=data)
+        self.__logger.info(model.__dir__())
+        # model.save_model()
