71bd99fdbe834af9cd012af0574d06206b989c44
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-07-15 23:26:01 +0100

    Update src/modelling/interface.py

--- src/modelling/interface.py
+++ src/modelling/interface.py
@@ -3,28 +3,20 @@ import logging
 
 import datasets
 
-import config
-import src.modelling.t5.steps
-
-
 
 class Interface:
     """
     An interface to one or more model development packages
     """
 
-    def __init__(self, source: datasets.Dataset):
+    def __init__(self, source: datasets.DatasetDict):
         """
 
-        :param source: A datasets.Dataset data piece for modelling
+        :param source: A datasets.DatasetDict for modelling
         """
 
         self.__source = source
 
-        # Splitting
-        self.__configurations = config.Config()
-        self.__splits: datasets.DatasetDict = self.__source.train_test_split(
-            test_size=self.__configurations.test_fraction)
 
         # Logging
         logging.basicConfig(level=logging.INFO,
@@ -38,16 +30,4 @@ class Interface:
         :return:
         """
 
-        temporary = self.__splits['test'].train_test_split(test_size=0.25)
-
-        cuts = datasets.DatasetDict({
-            'train': self.__splits['train'],
-            'valid': temporary['train'],
-            'test': temporary['test']
-        })
-        self.__logger.info(cuts.keys())
-        self.__logger.info('Training:\n%s', cuts['train'].shape)
-        self.__logger.info('Validating:\n%s', cuts['valid'].shape)
-        self.__logger.info('Testing:\n%s', cuts['test'].shape)
-
-        # src.modelling.t5.steps.Steps(splits=self.__splits).exc()
+        # src.modelling.t5.steps.Steps(splits=self.__source).exc()
