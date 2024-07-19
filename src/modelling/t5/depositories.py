5ae086a0541aba686a37722f628789ea04b4ddc6
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-07-19 10:49:25 +0100

    Update src/modelling/t5/depositories.py

--- src/modelling/t5/depositories.py
+++ src/modelling/t5/depositories.py
@@ -1,7 +1,13 @@
-
+"""Module depositories.py"""
 import src.functions.directories
 
 class Depositories:
+    """
+    Class Depositories
+
+    This class re-prepares output directories.  It will be
+    moved outwith src.modelling.t5
+    """
 
     def __init__(self):
         """
@@ -9,6 +15,11 @@ class Depositories:
         """
 
     def exc(self, path):
+        """
+
+        :param path: The directory path that will be re-created.
+        :return:
+        """
 
         directories = src.functions.directories.Directories()
         directories.cleanup(path=path)
