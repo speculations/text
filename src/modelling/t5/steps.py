2244208769f152a86c48bc053e36c8ee63865fde
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-08-13 13:21:39 +0100

    Update src/modelling/t5/steps.py

--- src/modelling/t5/steps.py
+++ src/modelling/t5/steps.py
@@ -5,6 +5,7 @@ import os
 import datasets
 import transformers
 
+import config
 import src.elements.variable as vr
 import src.modelling.t5.depositories
 import src.modelling.t5.intelligence
@@ -20,7 +21,7 @@ class Steps:
         """
 
         :param source: A dictionary of data splits; training, validation, etc., splits.
-        :param device:
+        :param device: A string denoting graphics or central processing unit, i.e., 'cuda' or 'cpu', respectively.
         """
 
         self.__source = source
@@ -28,16 +29,25 @@ class Steps:
 
         # A set of values for machine learning model development
         self.__variable = vr.Variable()
-        self.__variable = self.__variable._replace(EPOCHS=2)
-
-        # Preprocessing Instance: For tokenization.
-        self.__preprocessing = src.modelling.t5.preprocessing.Preprocessing(variable=self.__variable)
+        self.__variable = self.__variable._replace(
+            EPOCHS=2, MODEL_OUTPUT_DIRECTORY=os.path.join(config.Config().warehouse, 't5'))
 
         # Logging
         logging.basicConfig(level=logging.INFO,
                             format='\n\n%(message)s\n%(asctime)s.%(msecs)03d',
                             datefmt='%Y-%m-%d %H:%M:%S')
         self.__logger = logging.getLogger(__name__)
+
+    def __after_tokenization(self) -> datasets.DatasetDict:
+
+        # Preprocessing Instance: For tokenization.
+        preprocessing = src.modelling.t5.preprocessing.Preprocessing(variable=self.__variable)
+
+        # Converting each split into a T5 tokenized split
+        data: datasets.DatasetDict = self.__source.map(preprocessing.exc, batched=True)
+        self.__logger.info(f'{self.__source.keys()}\n{data.keys()}')
+
+        return data
 
     def exc(self):
         """
@@ -47,16 +57,13 @@ class Steps:
         """
 
         # Re-write
-        output_directory = os.path.join('warehouse', 't5')
-        src.modelling.t5.depositories.Depositories().exc(path=output_directory)
+        src.modelling.t5.depositories.Depositories().exc(path=self.__variable.MODEL_OUTPUT_DIRECTORY)
 
-        # Converting each split into a T5 tokenized split
-        data: datasets.DatasetDict = self.__source.map(self.__preprocessing.exc, batched=True)
-        self.__logger.info(self.__source.keys())
-        self.__logger.info(data.keys())
+        # The data, after tokenization
+        data = self.__after_tokenization()
 
         # Model
         intelligence = src.modelling.t5.intelligence.Intelligence(
-            variable=self.__variable, device=self.__device, output_directory=output_directory)
+            variable=self.__variable, device=self.__device)
         model: transformers.Seq2SeqTrainer = intelligence(data=data)
         self.__logger.info(dir(model))
