d468368485dff4d9d981c04e98f73fac4f6283f8
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-08-13 20:03:47 +0100

    Update src/modelling/t5/settings.py

--- src/modelling/t5/settings.py
+++ src/modelling/t5/settings.py
@@ -5,8 +5,16 @@ import ray.tune.schedulers as rts
 
 class Settings:
 
+    perturbation_interval = 2
+
     tune_by = {
         'per_device_train_batch_size': 32,
         'per_device_eval_batch_size': 32,
         'num_train_epochs': ray.tune.choice([2, 3, 4, 5])
     }
+
+    rts.PopulationBasedTraining(
+        time_attr='training_iteration',
+        perturbation_interval=perturbation_interval,
+
+    )
