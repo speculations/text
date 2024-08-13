0e5a4ed036d2695d41cd54f0eb9883e4ea7d55c1
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-08-13 23:25:00 +0100

    Update src/modelling/t5/settings.py

--- src/modelling/t5/settings.py
+++ src/modelling/t5/settings.py
@@ -19,7 +19,7 @@ class Settings:
 
         self.__perturbation_interval = 2
 
-        hp_space = {
+        self.hp_space = {
             'per_device_train_batch_size': 32,
             'per_device_eval_batch_size': 32,
             'num_train_epochs': ray.tune.choice([2, 3, 4, 5])
