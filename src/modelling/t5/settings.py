74f1be77a785c6cabe41780e83522cd6283f4337
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-08-13 21:45:15 +0100

    Update src/modelling/t5/settings.py

--- src/modelling/t5/settings.py
+++ src/modelling/t5/settings.py
@@ -6,6 +6,7 @@ import ray.tune.schedulers as rts
 class Settings:
 
     perturbation_interval = 2
+    lr = 0.1
 
     tune_by = {
         'per_device_train_batch_size': 32,
@@ -15,6 +16,17 @@ class Settings:
 
     rts.PopulationBasedTraining(
         time_attr='training_iteration',
+        metric='',
+        mode='max',
         perturbation_interval=perturbation_interval,
+        hyperparam_mutations={
+            'lr': ray.tune.qloguniform(lower=5e-3, upper=1e-1, q=5e-4),
+            'h0': ray.tune.uniform(lower=0.0, upper=1.0),
+            'h1': ray.tune.uniform(lower=0.0, upper=1.0)
+        },
+        quantile_fraction=0.25,
+        resample_probability=0.25
 
     )
+
+
