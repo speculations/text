e67217104c6298e7b5d874a4c01375328f301d17
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-08-14 22:47:21 +0100

    Update src/modelling/t5/skeleton.py

--- src/modelling/t5/skeleton.py
+++ src/modelling/t5/skeleton.py
@@ -29,7 +29,7 @@ class Skeleton:
                             datefmt='%Y-%m-%d %H:%M:%S')
         self.__logger = logging.getLogger(__name__)
 
-    def exc(self) -> transformers.models.t5.modeling_t5.T5ForConditionalGeneration:
+    def exc(self):
         """
 
         :return:
@@ -43,8 +43,8 @@ class Skeleton:
         self.__logger.info('max_new_tokens: %s', config.max_new_tokens)
 
         # Model initialisation
-        model: transformers.models.t5.modeling_t5.T5ForConditionalGeneration
-        model = transformers.AutoModelForSeq2SeqLM.from_pretrained(
-            pretrained_model_name_or_path=self.__parameters.checkpoint, config=config)
+        # model: transformers.models.t5.modeling_t5.T5ForConditionalGeneration
+        # model = transformers.AutoModelForSeq2SeqLM.from_pretrained(
+        #     pretrained_model_name_or_path=self.__parameters.checkpoint, config=config)
 
-        return model
+        return config
